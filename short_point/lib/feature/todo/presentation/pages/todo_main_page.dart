a460f3ef2d8e729f138fb990ce85c9430a168647
unknown <henokmekuanint79@gmail.com>
2023-11-02 20:27:27 +0300

    task widet add

--- short_point/lib/feature/todo/presentation/pages/todo_main_page.dart
+++ short_point/lib/feature/todo/presentation/pages/todo_main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:short_point/feature/todo/presentation/widgets/task_widget.dart';
 
 class TodoHomePage extends StatefulWidget {
   TodoHomePage({super.key});
@@ -11,42 +12,110 @@ class TodoHomePage extends StatefulWidget {
 class _TodoHomePageState extends State<TodoHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            color: Color(0XFF3556AB),
-            width: double.infinity,
-            height: 20.h,
-            child: Row(
-            children: [
-              CircleAvatar(
-                radius: 10.r,
-                child: Image.asset("assets/images/ProfilePicture.png"),
-              ),
-              Column(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0XFFF3F3F3),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              color: Color(0XFF3556AB),
+              width: double.infinity,
+              height: 123.h,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Hello,jhon"),
-                  Text("What are your plans today")
+                  Container(
+                    margin: EdgeInsets.only(left: 32.w, top: 19.h),
+                    child: CircleAvatar(
+                      radius: 25.r,
+                      child: Image.asset(
+                        "asset/images/ProfilePicture.png",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 24.h, left: 20.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Hello,jhon"),
+                        Container(
+                          width: 220.w,
+                          height: 52.h,
+                          child: Text("What are your plans today ?"),
+                        )
+                      ],
+                    ),
+                  )
                 ],
-              )
-            ],
-          ),
-          ),
-
-          Container(
-            width: double.infinity,
-            height: 20.h,
-            color: Color(0XFF9EB031),
-            child: Row(
-              children: [
-                Text("Go Pro (No Ads)"),
-                Text("No fuss, no ads, for only \$1 a month")
-              ],
+              ),
             ),
-          )
-        ],
+            Container(
+              width: double.infinity,
+              height: 130.h,
+              color: Color(0XFF9EB031),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 25.w, top: 36.h, right: 28.w),
+                    child: Image.asset(
+                      "asset/images/throphy.png",
+                      // width: 53.w,
+                      // height: 40.h,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 36.h,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("Go Pro (No Ads)"),
+                        Container(
+                          width: 190.w,
+                          height: 46.h,
+                          child: Text("No fuss, no ads, for only \$1 a month"),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 15.w),
+                    width: 66.w,
+                    height: 71.h,
+                    color: Color(0XFF071D55),
+                    child: Center(
+                      child: Text(
+                        "\$1",
+                        style: TextStyle(color: Color(0XFFF2C94C)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  itemCount: 14,
+                  itemBuilder: (context, index) {
+                    return TaskWidget(
+  taskName: "asd",
+  isCompleted: true,
+  onToggleCheckbox: (bool? newValue) {
+    // Add the logic to handle the toggling of the checkbox here
+  },
+);
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ short_point/lib/feature/todo/presentation/widgets/task_widget.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TaskWidget extends StatefulWidget {
+  final String taskName;
+  final bool isCompleted;
+  final ValueChanged<bool?> onToggleCheckbox;
+
+  const TaskWidget({
+    Key? key,
+    required this.taskName,
+    required this.isCompleted,
+    required this.onToggleCheckbox,
+  }) : super(key: key);
+
+  @override
+  _TaskWidgetState createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.w,vertical: 20.h),
+      padding: EdgeInsets.only(right: 15.w),
+      color: Colors.white,
+      height: 90.h,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            
+            width: 250.w,
+            child: Row(
+              
+              children: [
+                Container(
+            margin: EdgeInsets.only(left: 17.w,right: 30.w),
+            child: Theme(
+                  data: ThemeData(
+            unselectedWidgetColor: Colors.transparent,
+            checkboxTheme: CheckboxThemeData(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0), // Adjust the radius as needed
+              ),
+            ),
+            
+                  ),
+                  child: Transform.scale(
+                    scale: 2,
+                    child: Checkbox(
+                              
+                              value: widget.isCompleted,
+                              onChanged: widget.onToggleCheckbox,
+                              fillColor: MaterialStateProperty.resolveWith<Color>((states) {
+      if (states.contains(MaterialState.selected)) {
+        return const Color(0xFF399649); // Change the checkbox color here
+      }
+      return Colors.black; // Change the default color here
+    })
+                              
+                    ),
+                  ),
+                ),
+          ),
+
+
+
+      Text(
+        widget.taskName,
+        style: TextStyle(
+         decoration: widget.isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
+        ),
+      ),
+              ],
+            ),
+          ),
+
+ElevatedButton(
+  style: ElevatedButton.styleFrom(
+    fixedSize: Size(80.w, 60.h),
+    side: const BorderSide(width: 1.0, color: Colors.black), // Set border color
+    backgroundColor: Colors.white, // Set central color to white
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(6.r),
+    ),
+  ),
+  onPressed: () {
+    // Add your onPressed logic here
+  },
+  child: Text(
+    "Edit",
+    style: TextStyle(
+      fontSize: 16.sp,
+      color: const Color(0xFF3556AB), // Set text color
+    ),
+  ),
+),
+
+        ],
+      ),
+    );
+  }
+}
+
+

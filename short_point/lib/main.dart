6d5fd13b1db40dff1db46e8858468cc00ca13700
unknown <henokmekuanint79@gmail.com>
2023-11-02 18:39:07 +0300

    screen util add

--- short_point/lib/main.dart
+++ short_point/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:short_point/feature/todo/presentation/todo_main_page.dart';
 
 void main() {
@@ -8,17 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      
+      designSize: Size(390, 844),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        showSemanticsDebugger: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MyHomePage(),
       ),
-      home:  MyHomePage(),
     );
   }
 }
